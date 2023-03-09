6aa7213a79e06ea35046a07fcd37571c3cbcf8e7
joel.dygard <joel.dygard@synteda.se>
2023-03-09 16:10:23 +0100

    CRA installed

---
+++ fake/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import redux from 'redux';
+import reactRedux from 'react-redux';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" />
+    </div>
+  );
+}
+
+export default App;
